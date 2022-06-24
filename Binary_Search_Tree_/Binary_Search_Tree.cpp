0cc62f03a82b15231d0ff80df9adf90df9e0ff26
ArmanKhachatryan2001 <104670251+ArmanKhachatryan2001@users.noreply.github.com>
2022-06-24 18:50:50 +0400

    Update Binary_Search_Tree.cpp

--- Binary_Search_Tree_/Binary_Search_Tree.cpp
+++ Binary_Search_Tree_/Binary_Search_Tree.cpp
@@ -2,16 +2,3 @@
 #include <initializer_list>
 #include <vector>
 #include "BST.h"
-int main()
-{
-    BST<int> a = {50,30,35,23};
-    BST<int> b = {36,4,5,6,2,1};
-    BST<int> c = a + b;
-
-    //a.levelorder();
-    //a.inorder();
-    //std::cout << a.get_height();
-    //std::cout << std::endl;
-    std::cout << c;
-    //std::cout << a << std::endl;}
-}
