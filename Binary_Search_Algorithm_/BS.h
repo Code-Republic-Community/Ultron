80636160a3685594dcbab0a4a0d7a25cec589b1c
ArmanKhachatryan2001 <104670251+ArmanKhachatryan2001@users.noreply.github.com>
2022-06-24 19:17:21 +0400

    Update BS.h

--- Binary_Search_Algorithm_/BS.h
+++ Binary_Search_Algorithm_/BS.h
@@ -1,16 +1,16 @@
 #ifndef BS_H
 #define BS_H
 template <typename T>
-int BinarySearch(T* arr, int size, T value);
+int BinarySearch(T* arr, int size, T value); //for a simple array
 template<typename iter>
-int size(iter begin, iter end);
+int size(iter begin, iter end); // This function is for size determination
 template<typename iterator>
-iterator add(iterator it, int value);
+iterator add(iterator it, int value); // This function is intended for plus operation
 template<typename iterator, typename T>
-iterator BinarySearch(iterator begin, iterator end, T value, std::forward_iterator_tag);
+iterator BinarySearch(iterator begin, iterator end, T value, std::forward_iterator_tag); // This function works for list iterators
 template<typename iterator, typename T>
-iterator BinarySearch(iterator begin, iterator end, T value, std::random_access_iterator_tag);
+iterator BinarySearch(iterator begin, iterator end, T value, std::random_access_iterator_tag); // This function works for vector iterators
 template<typename IteratorType, typename T>
-IteratorType Binary_Search(IteratorType start, IteratorType end, T value);
+IteratorType Binary_Search(IteratorType start, IteratorType end, T value); // This function is designed to determine whether it is a list iterator or a vector iterator
 #include "BS.hpp"
 #endif
