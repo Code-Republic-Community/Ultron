4b06832f3bc9580d91759f663177e998ae206653
Aro MIlitosyan <ar.militosyan.97@mail.ru>
2022-06-26 22:52:30 +0400

    Update euclidsAlgorithm.cpp

--- Euclid-s-Algorithm/euclidsAlgorithm.cpp
+++ Euclid-s-Algorithm/euclidsAlgorithm.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 int  largest_divider(int,int);
 
 int main(){
@@ -13,8 +14,8 @@ int main(){
 }
 
 int  largest_divider(int one_num,int  two_num){
-  if (two_num == 0){
-    return one_num;
+if (two_num == 0){
+   return one_num;
   }
-    return largest_divider(two_num,(one_num%two_num) );
+ return largest_divider(two_num,(one_num%two_num) );
 }
