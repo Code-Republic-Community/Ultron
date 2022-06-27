05ed1a4931e18cde05e441cb8c52e96070b4b992
Aro MIlitosyan <ar.militosyan.97@mail.ru>
2022-06-27 10:44:21 +0400

    Update Max_heap.h

--- Heap  Data Structure/Max_heap.h
+++ Heap  Data Structure/Max_heap.h
@@ -8,7 +8,6 @@ void swap(T &x, T &y);
 template<typename T>
 class MaxHeap
 {
-
 private:
 std::vector<T> vec_heap;
 public:
@@ -19,16 +18,17 @@ public:
     MaxHeap(std::initializer_list<T> obj); // parameterized constructor
     ~MaxHeap();//destructor
  
-  MaxHeap& operator=(const MaxHeap<T>& heap);///copy operator  
-  MaxHeap& operator=( MaxHeap<T>&& heap); // move operator
+    MaxHeap& operator=(const MaxHeap<T>& heap);///copy operator  
+    MaxHeap& operator=( MaxHeap<T>&& heap); // move operator
 
-   bool operator==(const MaxHeap<T> equal){
-       if(vec_heap.size() != equal.vec_heap.size() ){
-           return false;
+  bool operator==(const MaxHeap<T> equal){
+   if(vec_heap.size() != equal.vec_heap.size() ){
+    return false;
        }
-       for(int i = 0;  i < vec_heap.size();++i){
-           if(vec_heap[i] != equal.vec_heap[i])
-           return false;
+       
+   for(int i = 0;  i < vec_heap.size();++i){
+    if(vec_heap[i] != equal.vec_heap[i])
+     return false;
        }
        return true;
    }///equal operator
@@ -38,68 +38,58 @@ public:
   }//noequal operator
 
   MaxHeap operator+=(const MaxHeap<T>& sumequal){
+  if(sumequal.vec_heap.size() == vec_heap.size()){
+   for(int i = 0; i < vec_heap.size();++i){
+    this->vec_heap[i] += sumequal.vec_heap[i];
+    }
     
+  }
      
-        if(sumequal.vec_heap.size() == vec_heap.size()){
-         for(int i = 0; i < vec_heap.size();++i){
-        this->vec_heap[i] += sumequal.vec_heap[i];
-          
-            }
-         }
-       
-      if(sumequal.vec_heap.size() < vec_heap.size())
-      for(int i = 0; i < sumequal.vec_heap.size();++i){
-      this->vec_heap[i] += sumequal.vec_heap[i];
-    
+  if(sumequal.vec_heap.size() < vec_heap.size())
+   for(int i = 0; i < sumequal.vec_heap.size();++i){
+     this->vec_heap[i] += sumequal.vec_heap[i];
         }
         
-        
-        if(sumequal.vec_heap.size() > vec_heap.size()){
-        for(int i = 0; i < vec_heap.size();++i){
-     
+    if(sumequal.vec_heap.size() > vec_heap.size()){
+     for(int i = 0; i < vec_heap.size();++i){
       this->vec_heap[i] += sumequal.vec_heap[i];
         }
-       for(int i = vec_heap.size();i < sumequal.vec_heap.size();++i) {
-          
-            insert(sumequal.vec_heap[i]);
-        }
         
-        }
-       
-        
+   for(int i = vec_heap.size();i < sumequal.vec_heap.size();++i) {
+     insert(sumequal.vec_heap[i]);
+      }
+   	}  
       return *this;
   }; //sumequal operator
     
   MaxHeap operator+( const MaxHeap<T>& sum){
-     MaxHeap <T> tmp;
-      if(sum.vec_heap.size() > vec_heap.size()){
-          for(int i = 0; i < sum.vec_heap.size();++i){
-          tmp.vec_heap.push_back(vec_heap[i] + sum.vec_heap[i]);
+   MaxHeap <T> tmp;
+  if(sum.vec_heap.size() > vec_heap.size()){
+   for(int i = 0; i < sum.vec_heap.size();++i){
+    tmp.vec_heap.push_back(vec_heap[i] + sum.vec_heap[i]);
          }
       }
-         if(sum.vec_heap.size() < vec_heap.size()){
-               for(int i = 0; i < sum.vec_heap.size();++i){
-          tmp.vec_heap.push_back(vec_heap[i] + sum.vec_heap[i]);
-          
-         }
-         
-          for(int i = sum.vec_heap.size();i < vec_heap.size();++i) {
-            tmp.vec_heap.push_back(vec_heap[i]);
+  if(sum.vec_heap.size() < vec_heap.size()){
+   for(int i = 0; i < sum.vec_heap.size();++i){
+    tmp.vec_heap.push_back(vec_heap[i] + sum.vec_heap[i]);
+      }
+   for(int i = sum.vec_heap.size();i < vec_heap.size();++i) {
+     tmp.vec_heap.push_back(vec_heap[i]);
         }
-         }
-         if(sum.vec_heap.size() == vec_heap.size()){
-         for(int i = 0; i < vec_heap.size();++i){
-             tmp.vec_heap.push_back(vec_heap[i] + sum.vec_heap[i]);
-            }
-         }
+       }
+ if(sum.vec_heap.size() == vec_heap.size()){
+   for(int i = 0; i < vec_heap.size();++i){
+     tmp.vec_heap.push_back(vec_heap[i] + sum.vec_heap[i]);
+       }
+     }
            return tmp;
     };
    // sum operator
     
   friend std::ostream& operator<<(std::ostream& out ,const MaxHeap<T> &obj) {
-      for(int i = 0;i < obj.size();++i){
+  for(int i = 0;i < obj.size();++i){
       out << obj.vec_heap[i] << "  ";
-      }
+    }
       return out;
   };//cout  << operator
 
@@ -126,8 +116,10 @@ public:
 	void delete_element(T value_delete);
 public:
 	void insert(T value);
+	
 	void print();
-int linear_search(T value_search);
+	
+  int linear_search(T value_search);
 	
 	 
 };
