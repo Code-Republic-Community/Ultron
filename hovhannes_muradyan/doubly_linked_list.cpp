1052489807fdf393fec159eda37076e5a3638890
hovomuradyan <hmuradyan008@gmail.com>
2022-08-31 14:45:55 +0400

    methods and operators

--- hovhannes_muradyan/doubly_linked_list.cpp
+++ hovhannes_muradyan/doubly_linked_list.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-using namespace std;
 
+using namespace std;
 
 class node{
 public:
@@ -25,6 +25,62 @@ public:
   ~node()= default;
 };
 
+ostream& operator<<(ostream& COUT, node *head){
+  node* temp=head;
+  while(temp!=nullptr){
+    COUT<<temp->data<<" --> ";
+    temp=temp->next;
+  }
+  cout<<"NULL"<<endl;
+  return COUT;
+}
+
+bool operator==(node& head1, node& head){
+  if((head1.next == head.next) && (head1.prev == head.prev) && (head1.data == head.data)){
+    return true;
+  }
+  return false;
+}
+bool operator!=(node& head1, node& head){
+  if((head1.next == head.next) && (head1.prev == head.prev) && (head1.data == head.data)){
+    return false;
+  }
+  return true;
+}
+bool operator>(node& head1, node& head){
+  if(head1.data > head.data){
+    return true;
+  }
+  return false;
+}
+bool operator>=(node& head1, node& head){
+  if(head1.data >= head.data){
+    return true;
+  }
+  return false;
+}
+bool operator<(node& head1, node& head){
+  if(head1.data < head.data){
+    return true;
+  }
+  return false;
+}
+bool operator<=(node& head1, node& head){
+  if(head1.data <= head.data){
+    return true;
+  }
+  return false;
+}
+void operator+(node& head1, node& head){
+  head1.next = &head;
+  head.prev = &head1;
+}
+
+void operator+=(node& head1, node& head){
+  head1.next = &head;
+  head.prev = &head1;
+}
+
 int front(node* head){
   node* temp = head;
   while(temp->prev != nullptr){
@@ -41,6 +97,22 @@ int back(node* head){
   return(temp->data);
 }
 
+node* begin(node* head){
+  node* temp = head;
+  while(temp->prev != nullptr){
+    temp = temp->prev;
+  }
+  return(temp);
+}
+
+node* end(node* head){
+  node* temp= head;
+  while(temp->next != nullptr){
+    temp = temp->next;
+  }
+  return(temp);
+}
+
 int size(node* head){
   node* temp = head;
   int counter = 1;
@@ -52,15 +124,19 @@ int size(node* head){
 }
 
 void push_front(node* &head, int value){
-
   node* n = new node(value);
   n->next=head;
-
   if(head!=nullptr){
     head->prev=n;
   }
-
   head=n;
+}
+
+bool empty(node* &head){
+  if(head == nullptr){
+    return true;
+  }
+  return false;
 }
 
 void push_back(node* &head, int value)
@@ -78,28 +154,73 @@ void push_back(node* &head, int value)
   n->prev=temp;
 }
 
-void print(node* head){
-  node* temp=head;
-  while(temp!=nullptr){
-    cout<<temp->data<<" --> ";
-    temp=temp->next;
+void insert(node* &head, int index, int value){
+  if(size(head) < index){
+    push_back(head,value);
   }
-  cout<<"NULL"<<endl;
+  else if(index < 0){
+    push_front(head, value);
+  }
+  else{
+    node* temp=head;
+    int counter = 0;
+    while(counter < index){
+      counter++;
+      temp=temp->next;
+    }
+    node* n = new node(temp, value, temp->next);
+    temp->next = n;
+    (temp->next)->prev = n;
+  }
+}
+
+void emplace(node* &head, int index1, int index2){
+  node* tmp = head;
+  int value1 = 0;
+  int value2 = 0;
+  int counter = 0;
+  while((tmp->next) != nullptr){
+    if(counter == index1){
+      value1 = tmp->data;
+    }
+    else if(counter == index2){
+      value2 = tmp->data;
+    }
+    else if(counter > index2 && counter > index1){
+      break;
+    }
+    counter++;
+    tmp = tmp->next;
+  }
+  tmp = head;
+  counter = 0;
+  while((tmp->next) != nullptr){
+    if(counter == index1){
+      tmp->data = value2;
+    }
+    else if(counter == index2){
+      tmp->data = value1;
+    }
+    else if(counter > index2 && counter > index1){
+      break;
+    }
+    counter++;
+    tmp = tmp->next;
+  }
+  tmp = begin(tmp);
+  head = tmp;
 }
 
 int main()
 {
-  node* head1 = new node(1);
-  node* head2 = new node(2);  
-  node* head = new node(head1, 1, head2);
+  node* head = nullptr;
+  node* head1 = new node(head, 2, nullptr);
   push_back(head,1);
   push_back(head,2);
   push_back(head,3);
   push_back(head,4);
   push_back(head,5);
   push_back(head,6);
-  print(head);
-  cout<< front(head) << endl;
-  cout << back(head) << endl;
-  cout << size(head) << endl;
+  emplace(head, 1, 2);
+  cout << head << endl;
 }
