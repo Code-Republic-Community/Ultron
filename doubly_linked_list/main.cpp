05fad9906cb4958637e48f7db8b1b3ce013cdf4e
hovomuradyan <88653435+hovomuradyan@users.noreply.github.com>
2022-12-05 18:07:54 +0400

    Update main.cpp

--- doubly_linked_list/main.cpp
+++ doubly_linked_list/main.cpp
@@ -1,20 +1,38 @@
+#include <iostream>
+#include <algorithm>
 #include "List.h"
 
-bool is_odd(int value) {
-  if(value%2 == 1){
-    return true;
-  }
-  return false;
-}
-
-int main(){
-  List<int> a(1);
-  a.push_front(2);
-  a.push_back(3);
-  a.push_back(6);
-  a.push_front(11);
-  a.insert(4, 74);
-  List<int> b = {2, 2, 1, 1, 4, 4, 4, 5, 5, 2};
-  std::cout << b << std::endl;
-  std::cout << a << std::endl;
+int main() {
+  //TESTING::constructors
+  std::cout << "Testing constructors:" << std::endl;
+  List<int> first(1);
+  List<int> second = {2, 2, 1, 1, 4, 4, 4, 5, 5, 2, 6};
+  List<int>& forth(second);
+  List<int> third;
+  std::cout << first;
+  std::cout << second;
+  std::cout << forth;
+  std::cout << third;
+  std::cout << std::endl;
+  //TESTING::comparison
+  std::cout << "Testing comparison operators:" << std::endl;
+  std::cout << (forth == second) << std::endl;
+  std::cout << (first != third) << std::endl;
+  std::cout << (forth > first) << std::endl;
+  std::cout << (third < first) << std::endl;
+  std::cout << (forth >= second) << std::endl;
+  std::cout << (second <= forth) << std::endl;
+  std::cout << std::endl;
+  //TESTING::returnMethods
+  std::cout << "Testing value-return methods:" << std::endl;
+  std::cout << (first + third);
+  std::cout << second.size() << std::endl;
+  std::cout << second.front() << std::endl;
+  std::cout << second.back() << std::endl;
+  std::cout << third.empty() << std::endl;
+  std::cout << std::endl;
+  //Testing::actions
+  std::cout << "Testing actions:" << std::endl;
+  third += first;
+  //TODO::actionMethods
 }
