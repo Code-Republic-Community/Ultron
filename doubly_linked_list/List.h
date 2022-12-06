be81fe7753d33a234bc09ae295069efdd71196b2
hovomuradyan <88653435+hovomuradyan@users.noreply.github.com>
2022-12-06 15:39:12 +0400

    Update List.h

--- doubly_linked_list/List.h
+++ doubly_linked_list/List.h
@@ -17,26 +17,26 @@ public:
   template<typename U>
   friend std::ostream &operator<<(std::ostream &out, const List<U> &obj);
   List<T> &operator=(List<T> other_list);
-  bool operator!=(List<T> &obj);
-  bool operator==(List<T> &obj);
-  bool operator>(List<T> &obj);
-  bool operator>=(List<T> &obj);
-  bool operator<(List<T> &obj);
-  bool operator<=(List<T> &obj);
-  void operator+=(List<T> obj);
-  List<T> operator+(List<T> obj);
+  bool operator!=(const List<T> &obj) const;
+  bool operator==(const List<T> &obj) const;
+  bool operator>(const List<T> &obj) const;
+  bool operator>=(const List<T> &obj) const;
+  bool operator<(const List<T> &obj) const;
+  bool operator<=(const List<T> &obj) const;
+  void operator+=(const List<T> &obj);
+  List<T> operator+(const List<T> &obj) const;
 
 public:
-  int size() const; // return count of elements in list
+  [[nodiscard]] int size() const; // return count of elements in list
   T front() const; // return first element value
   T back() const; // return last element value
   void push_front(T value); // add element at front
   void push_back(int value); // append element
-  void emplace(int index1, int index2); // emplace element values by index
-  void resize(int index); // save first index elements
+  void emplace(int index1, int index2); // replace element values by index
+  void resize(int index); // cut or add zeros to change the size of the list
   void insert(int index, T value); // insert element with value in index
   void clear(); // clear all list
-  bool empty() const; // check if list is empty or not
+  [[nodiscard]] bool empty() const; // check if list is empty or not
   void unique(); // remove duplicate values
   void merge(const List<T> &obj); // merge two list
   void splice(List<T> &obj); // append all elements from second list and delete from it
@@ -47,21 +47,26 @@ private:
     node *prev;
     node *next;
     T value;
+
     explicit node(T init_value);
     node(const node &init_node);
   };
+
   void remove_by_index(int index); //remove element by index
   node *_head;
   node *_tail;
-  class iterator  {
+
+  class iterator {
     typedef std::bidirectional_iterator_tag iterator_category;
     friend class List;
+
   public:
     iterator() : new_node(nullptr) {}
     bool operator!=(const iterator &itr) const;
     T &operator*() const {
       return new_node->value;
     }
+
     node *operator->() const;
     const iterator operator++(int);
     const iterator operator--(int);
@@ -74,8 +79,10 @@ private:
 public:
   iterator begin() const;
   iterator end() const;
+
   void remove_if(iterator start, iterator end, bool (*func)(T));
 };
 
 #include "List.hpp"
+
 #endif
