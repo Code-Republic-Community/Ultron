992e332e767deff032e5358fe0bb01b2fd7dc3cf
hovomuradyan <88653435+hovomuradyan@users.noreply.github.com>
2022-12-06 14:26:46 +0400

    Update main.cpp

--- doubly_linked_list/main.cpp
+++ doubly_linked_list/main.cpp
@@ -26,13 +26,47 @@ int main() {
   //TESTING::returnMethods
   std::cout << "Testing value-return methods:" << std::endl;
   std::cout << (first + third);
+  //std::cout << (second + forth); //TODO::FixTheBag
   std::cout << second.size() << std::endl;
   std::cout << second.front() << std::endl;
   std::cout << second.back() << std::endl;
   std::cout << third.empty() << std::endl;
   std::cout << std::endl;
-  //Testing::actions
+  //Testing::actionMethods
   std::cout << "Testing actions:" << std::endl;
   third += first;
-  //TODO::actionMethods
+  std::cout << third;
+  //third += forth; // TODO::FixTheBag
+  //std::cout << third;
+  first.clear();
+  std::cout << first;
+  first.push_back(321);
+  first.push_front(123);
+  first.emplace(0, 1);
+  first.resize(3);
+  first.insert(1, 321);
+  first.unique();
+  first.merge(third);
+  std::cout << first;
+  std::cout << forth;
+  first.splice(forth);
+  std::cout << first;
+  std::cout << forth;
+  forth.splice(first, 5); // TODO:ASK
+  std::cout << first;
+  std::cout << forth;
+  std::cout << std::endl;
+  //Testing::iterators
+  std::cout << "Testing iterators:" << std::endl;
+  for (auto pos = first.begin(); pos != first.end(); pos++) {
+    std::cout << pos->value << " ";
+  }
+  std::cout << std::endl << std::endl;
+  //Testing:endResults
+  std::cout << "Final tests:" << std::endl;
+  std::cout << first;
+  std::cout << second;
+  std::cout << third;
+  std::cout << forth;
+  std::cout << "All is good." << std::endl;
 }
