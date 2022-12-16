ff2b933791bffd837ce9d510a5dec6518c9c08cc
hovomuradyan <hmuradyan008@gmail.com>
2022-12-16 17:38:40 +0400

    Adding Makefile

--- doubly_linked_list/src/list.h
+++ doubly_linked_list/src/list.h
@@ -1,5 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
+#pragma once
 
 template<typename T>
 class List {
--- doubly_linked_list/tests/main.cpp
+++ doubly_linked_list/tests/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
 
-#include "list.h"
+#include "../src/list.h"
 
 int main() {
   //TESTING::constructors
